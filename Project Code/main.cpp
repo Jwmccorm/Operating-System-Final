07bf963b5649a592a4d54aa27324feb2bc10cc1f
jamcco <jwmccorm@lakeheadu.ca>
2019-11-05 16:33:05 -0500

    main -> Example of user input
    Using user input to cause the .h files to fire and deal with the command

--- Project Code/main.cpp
+++ Project Code/main.cpp
@@ -11,11 +11,28 @@ using namespace std;
 
 int main(){
 
-    string input = " ";
+    string command,remainder;
 
     cout << "Welcome! Please Input Instructions" << endl;
 
-    cin >> input;
+    //      Catching help command off of root
+    cin >> command;
+    if (command != "\\h"){
+        cin >> remainder;
+    }
 
-    cout << input;
+    //      Commands switch statement (No switch statement for strings)
+    if  (command == "copy") copyFile();
+    else if (command == "delete") deleteFile();
+    else if (command == "move") moveFile();
+    else if (command == "rename") renameFile();
+    else if (command == "modify") textModification();
+    else if(command == "\\h"){
+        cout << "HELP";
+    }
+
+    else{
+        cout << "Invalid Command use \"\\h\" for help";
+    }
+
 }
