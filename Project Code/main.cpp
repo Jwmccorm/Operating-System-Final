4109dd42fa7966590dae273ee0eb448a027e2877
jamcco <jwmccorm@lakeheadu.ca>
2019-11-12 12:39:13 -0500

    Updating the path name to "path" for the string variable
    Making reading easier as per Teks thoughts

--- Project Code/main.cpp
+++ Project Code/main.cpp
@@ -18,8 +18,8 @@ int main(){
         This fist section is just for user input of the string
     */
     cout << "Welcome! Please Input Instructions" << endl;
-    string directoryString = current_working_directory();
-    DIR *dr = opendir(directoryString.c_str());
+    string path = current_working_directory();
+    DIR *dr = opendir(path.c_str());
     string command;
     string remainder;
     string userInput;
@@ -27,13 +27,13 @@ int main(){
     while(1){
         command = "";
         remainder = "";
-        cout << directoryString + ">";
+        cout << path + ">";
 
         //      Catching help command off of root
         getline(cin, userInput);
 
         if (!userInput.compare("-h")){
-            cout << "THIS WILL DISPLAY THE HELP OPTIONS";
+            cout << "THIS WILL DISPLAY THE HELP OPTIONS" << endl;
             continue;
         }
         else if(!userInput.compare("exit")){
@@ -43,7 +43,7 @@ int main(){
             if(!userInput.compare("dir")){
                 command = "dir";
             }else{
-                cout << "Invalid Command, you need spaces, use /h for help";
+                cout << "Invalid Command, you need spaces, use /h for help" << endl;
                 continue;
             }
         }else{
@@ -54,14 +54,14 @@ int main(){
         /*
             Bulk of the information for the problem it self, use this to link up commands.
         */
-        if  (!command.compare("copy")) cout << copyFile(remainder, directoryString);
+        if  (!command.compare("copy")) cout << copyFile(remainder, path);
         else if (!command.compare("delete")) deleteFile();
         else if (!command.compare("move")) moveFile();
         else if (!command.compare("rename")) renameFile();
         else if (!command.compare("modify")) textModification();
         else if (!command.compare("-h"));
-        else if (!command.compare("cd"))  directoryString = directoryChange(remainder, directoryString);
-        else if (!command.compare("dir")) changeDirectory(directoryString);
+        else if (!command.compare("cd"))  path = directoryChange(remainder, path);
+        else if (!command.compare("dir")) changeDirectory(path);
         else{
             cout << "Invalid Command, you need spaces, use /h for help";
         }
