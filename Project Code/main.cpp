ef2607edaeb6afa7a154782645f1cad6b2b570fe
Nicholas Pinney <47693575+npinnifer@users.noreply.github.com>
2019-11-14 11:49:41 -0500

    Revert "Merge branch 'master' of https://github.com/Jwmccorm/Operating-System-Final"
    This reverts commit adcc8c703e9a0790e0fa1c67ba0506d9b63ee8ee, reversing
    changes made to 49d299ef2186df13d0aa1f45787d5cd1afcf6580.

--- Project Code/main.cpp
+++ Project Code/main.cpp
@@ -43,7 +43,7 @@ int main(){
             if(!userInput.compare("dir")){
                 command = "dir";
             }else{
-                cout << "Invalid Command, you need spaces, use -h for help" << endl;
+                cout << "Invalid Command, you need spaces, use /h for help" << endl;
                 continue;
             }
         }else{
@@ -63,7 +63,7 @@ int main(){
         else if (!command.compare("cd"))  path = directoryChange(remainder, path);
         else if (!command.compare("dir")) changeDirectory(path);
         else{
-            cout << "Invalid Command, you need spaces, use -h for help" << endl;
+            cout << "Invalid Command, you need spaces, use /h for help";
         }
 
     }
