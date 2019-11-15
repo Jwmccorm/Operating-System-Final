c501bd7fecb53b08b7dfd0bec7345b42f65d926e
PrabhjotmSingh <57496302+PrabhjotmSingh@users.noreply.github.com>
2019-11-14 21:10:08 -0500

    Update testDeleteFile.cpp
    hello everyone really sorry for not attending today's meeting. really apologize because I stucked in the store for some reason. And James can you please go through my work, if you don't mind. please and thank you.

--- Project Code/Features/DeleteFile/testDeleteFile.cpp
+++ Project Code/Features/DeleteFile/testDeleteFile.cpp
@@ -1,12 +1,35 @@
 //PRABHJOT SINGH MATHARU
 //0860228
 
-#include<iostream>
-#include "deletefile.h"
+public static void delete()
+	{
+		System.out.print("Enter the file name you want to delete: ");
+		String filename = in.nextLine();
 
-using namespace std;
+		String help = "Selected help\r\n" +
+				"The user will be asked to enter a file name to be deleted.\r\n" +
+				"If the file exists, it will be deleted.\r\n" +
+				"Otherwise, an error message will appear.\r\n";
+	//Java File delete() method can be used to delete files.
+    // Java file delete method returns true if file gets deleted
+    // and returns false if file doesn’t exist.
 
-int main(){
+		while(filename.equals("/h"))
+		{
+			System.out.println(help);
+			System.out.print("Enter the file name you want to delete: ");
+			filename = in.nextLine();
+		}
 
-    deleteFile();
-}
+
+		if (new File(filename).delete() == false)
+		{
+			System.out.println("unable to delete the selected file becuz of some error.");
+		}
+		else
+			System.out.println("The file has been deleted.");
+	}
+
+
+
+
