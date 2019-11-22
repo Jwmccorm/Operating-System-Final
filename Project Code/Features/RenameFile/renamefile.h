e7437483fea8902c2fd763492e39b02a6b6be2d6
TekSubedi <47825415+TekSubedi@users.noreply.github.com>
2019-11-22 08:40:40 -0500

    Takingcare of extension-but may not be 100%

--- Project Code/Features/RenameFile/renamefile.h
+++ Project Code/Features/RenameFile/renamefile.h
@@ -5,80 +5,118 @@
 #include <cstring>
 using namespace std;
 
-int myRename(char curName[], char nName[])
+// function returning the extension of the file dot inclusive
+string checker(char curName[])
 {
-    int i;
+    // local declarations and assignments
+    int i, temp, tempIndex, dotCount =0 ;
+    string str;
+    // for loop to find the the last dot that is the starting of extension
     for(int i = 0; i<strlen(curName); i++)
     {
-        if(i<strlen(nName))
-            curName[i] = nName[i];
-        else
-            curName[i] = '\0';
+        // finds the dot and saves the index of the last dot in temp
+        if(int(curName[i])== 46)
+        {
+            temp = i;
+        }
     }
-    cout<<curName<<endl;
-    cout<<nName<<endl;
-    if(strcmp(curName, nName) == 0)
+    // storing the extension in the string called str
+    for(i = temp; i<strlen(curName); i++)
     {
-        return 0;
+        str = str + curName[i];
     }
-    else
-        return -1;
-
+    cout<<"making sure that original extension is saved "<<str<<endl;
+    return str;
 }
-void checker(char curName[])
+// assigning the old extension if user puts the new extension as extension can't be changed
+void keepExtension(char curName[], string oldExtension)
+{
+    // local declaration and assignments
+    int i, temp, j =0;
+    int n = oldExtension.length();
+    char tempAr[n];
+    // copying the string into array
+    strcpy(tempAr, oldExtension.c_str());
+    // putting the original extension to the new file name
+    for(i = strlen(curName); i<(strlen(curName) + n); i++)
+    {
+        curName[i] = tempAr[j++];
+    }
+    cout<<"making sure that original extension is added to new name "<<curName<<endl;
+}
+// removing the extension if user entered
+void removeExt(char curName[])
 {
     int i, temp ;
-    string str = " ";
-    for(int i = 0; i<strlen(curName); i++)
+    for(i = 0; i<strlen(curName); i++)
     {
         if(int(curName[i])== 46)
-            temp = i;
-        if(i >= temp)
         {
-            curName[i] = '\0';
+            temp = i;
         }
-
     }
-
+    // removing the extension
+    for(i = temp; i<strlen(curName); i++)
+    {
+        curName[i] = '\0';
+    }
 }
 
 // function to rename the desired file in the current directory
-void renameFile()
+string renameFile(string command, string path)
 {
     // local declarations and assignments
-     int tempInt, index = 30;
-     char tempArray[index], temp1[index];
-        // arrays to hold current name and new name
+    int tempInt, index = 30;
+    int i;
+    if (!command.compare("-h")){
+        return "To use the rename command:  \" file.extension then newname\"";
+    }
+    char tempArray[index], temp1[index];
+    // arrays to hold current name and new name
     char currentName[index],newName[index];
-
+    // string for current and new name
+    string cName, nName;
+    // string to hold old and new(if user enters) extension
+    string cExtension, nExtension;
 
     // prompt to user to give the name of the file they wish to rename
     cout<<"Please enter the name of the file you wish to rename."<<endl;
     cin>>currentName;
+    // saving the old extension to be used for new name
+    cExtension = checker(currentName);
     // access return is -ve, i.e. file doesn't exist
     while(access(currentName, 0)== -1)
         {
             cout<< currentName<<" The file does not exist.\nPlease enter valid file name to continue OR \nEnter Return to go back to main menu.\n"<<endl;
             cin>>currentName;
+            // saving the old extension to be used for new name
+            cExtension = checker(currentName);
             // return if user want to return
             if (strcmp(currentName,"Return")==0)
-                return;
+                return "";
             }
     // if file exists then renaming
     if(access(currentName, 0)!= -1)
     {
         cout<<"Please enter the new name for the file named "<<currentName<<endl;
         cin>>newName;
-        checker(newName);  //??????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????
+        // disregarding user's extension
+        removeExt(newName);
+        // keeping the original extension
+        keepExtension(newName, cExtension);
 
         //if acess return is not -ve i.e. this file name already exists in the dir so getting another desired name
          while(access(newName, 0)!= -1)
             {
                 cout<<newName<<" already exists.\nPlease enter valid file name to continue OR \nPlease Enter Return to go back to main menu."<<endl;
                 cin>>newName;
+                // disregarding user's extension
+                removeExt(newName);
+                // keeping the original extension
+                keepExtension(newName, cExtension);
                 // return if user want to return
                 if ((strcmp(newName,"Return"))==0)
-                    return;
+                    return "";
             }
         // storing return value of rename function
         tempInt = rename(currentName, newName);
@@ -86,44 +124,23 @@ void renameFile()
         // if tempInt is 0, i.e. file successfully renamed
         if(tempInt == 0)
         {
-            cout<< currentName << "file is successfully renamed to " <<newName<<endl;
+            cout<< currentName << "File is successfully renamed to  " <<newName<<endl;
             cout<< "Please Enter Return to go back to main menu."<<endl;
             cin>>tempArray;
             // for catching the invalid return entry
             tempInt = (strcmp(tempArray, "Return"));
             while(tempInt!= 0)
             {
-                cout<<"Invalid entry, Please enter Return to go back to main menu. "<<endl;
+                cout<<"Invalid entry, Please enter Return to go back to main menu."<<endl;
                 cin>>tempArray;
                 tempInt = (strcmp(tempArray, "Return"));
             }
             // retrun when the user enters Return
             if(tempInt == 0)
-                return;
+                return "";
         }
 
     }
+    return " ";
 }
 
-
-/*
-        //tempInt = myRename(currentName, newName);----- my rename is not working why
-        //?????????????????????????????????
-        OR even here without calling function name
-
-    for(int i = 0; i<strlen(currentName); i++)
-    {
-        if(i<strlen(newName))
-            currentName[i] = newName[i];
-        else
-            currentName[i] = '\0';
-    }
-    cout<<currentName<<endl;
-    cout<<newName<<endl;
-    if(strcmp(currentName, newName) == 0)
-    {
-        tempInt = 0;
-    }
-    else
-        tempInt = -1;
-        */
